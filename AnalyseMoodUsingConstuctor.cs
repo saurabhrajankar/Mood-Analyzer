9a8568926eddf9d1d00055871b25d43e92104e75
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-27 23:37:37 +0530

    AnalyseMoodUsingConstuctor

---
+++ AnalyseMoodUsingConstuctor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer
+{
+    public class AnalyzeMoodUsingConstructor
+    {
+        public string msg1= "i am sad";
+        public AnalyzeMoodUsingConstructor(string message)
+        {
+            this.msg1 = message;
+        }
+        public AnalyzeMoodUsingConstructor()
+        {
+        }
+        public string AnalyseMoodMethod1()
+        {
+            //if msg passed in constructor contains given value
+            if (msg1.ToUpper().Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ AnalyseMoodUsingConstuctorTest.cs
@@ -0,0 +1,29 @@
+﻿using Mood_Analyzer;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyze_Test
+{
+    [TestClass]
+    public class UnitTAnalyseMoodTest2
+    {
+        [TestMethod]
+        public void AnalyseMoodMethodTest()
+        {
+            AnalyzeMoodUsingConstructor a = new AnalyzeMoodUsingConstructor("i m sad");
+            string value = a.AnalyseMoodMethod1();
+            Assert.AreEqual("SAD", value);
+
+            AnalyzeMoodUsingConstructor b = new AnalyzeMoodUsingConstructor();
+            string z = b.AnalyseMoodMethod1();
+            Console.WriteLine("Using Default Constructor  " + z);
+            Assert.AreEqual("SAD", value);
+        }
+        
+
+    }
+}
