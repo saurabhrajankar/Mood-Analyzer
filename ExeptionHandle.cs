0c5a980dd64f5ab3805515ffef5b9fc57d1a56b6
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-27 23:39:03 +0530

    ExeptionHandled

---
+++ ExeptionHandle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer
+{
+    public class MoodAnalyzerException
+    {
+        public string MoodAnalyzerExceptionMethod(string message)
+        {
+            //exception handled using try catch
+            try
+            {
+                //checking if msg passed contains given value
+                string message1 = "SAD";
+                string message2 = "HAPPY";
+                if (message.ToUpper().Contains(message1.ToUpper()))
+                {
+                    return message1;
+                }
+                else if (message.ToUpper().Contains(message2.ToUpper()))
+                {
+                    return message2;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "HAPPY";
+            }
+            return null;
+        }
+    }
+}
---
+++ ExeptionHandleTest.cs
@@ -0,0 +1,21 @@
+﻿using Mood_Analyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyze_Test
+{
+    [TestClass()]
+    public class MoodAnalyzerExceptionTests
+    {
+        [TestMethod()]
+        public void MoodAnalyzerExceptionMethodTest()
+        {
+            MoodAnalyzerException a3 = new MoodAnalyzerException();
+            string mood3 = a3.MoodAnalyzerExceptionMethod(null);
+            Assert.AreEqual("HAPPY", mood3);
+        }
+    }
+}
