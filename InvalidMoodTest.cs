53b3de5c455e6ec107687d8613ac1fc06f249baa
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-27 23:40:29 +0530

    check  enter user invalid

---
+++ InvalidMoodTest.cs
@@ -0,0 +1,43 @@
+﻿using Mood_Analyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyze_Test
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        public MAP3 map3;
+        [TestMethod]
+        public void EmptyMsg()
+        {
+            map3 = new MAP3();
+            try
+            {
+                string msg = "";
+                string value = map3.analysemood(msg);
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Empty Value", e.Message);
+            }
+        }
+        [TestMethod]
+        public void NullMsg()
+        {
+            map3 = new MAP3();
+            try
+            {
+                string msg = null;
+                string value = map3.analysemood(msg);
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Null Value", e.Message);
+            }
+        }
+    }
+}
